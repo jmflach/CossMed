1ee9b77bab6e67dd3c1e7100bdf9eb956c71f054
jmflach <joaoflach@gmail.com>
2024-03-19 11:50:18 -0300

    added the delete doctor endpoint

--- src/main/java/coss/med/CossMed/controller/DoctorController.java
+++ src/main/java/coss/med/CossMed/controller/DoctorController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,8 +31,6 @@ public class DoctorController {
 	@Transactional
 	@PostMapping
 	public void register(@RequestBody @Valid DoctorDataDTO body) {
-		System.out.println(body);
-		
 		repository.save(new Doctor(body));
 	}
 	
@@ -42,12 +42,13 @@ public class DoctorController {
 	@Transactional
 	@PutMapping
 	public void update(@RequestBody @Valid DoctorUpdateDataDTO body) {
-		System.out.println("Updating");
-		
 		var doctor = repository.getReferenceById(body.id());
-		
 		doctor.updateData(body);
-		
-		System.out.println(doctor.getName());
+	}
+	
+	@Transactional
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
