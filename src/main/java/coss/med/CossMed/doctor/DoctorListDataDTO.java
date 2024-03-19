364b98b60585f1ed258ae9c00c514c73c4f903e1
jmflach <joaoflach@gmail.com>
2024-03-19 10:12:21 -0300

    added the id to the list doctor DTO

--- src/main/java/coss/med/CossMed/doctor/DoctorListDataDTO.java
+++ src/main/java/coss/med/CossMed/doctor/DoctorListDataDTO.java
@@ -1,11 +1,12 @@
 package coss.med.CossMed.doctor;
 
 public record DoctorListDataDTO(
+		Long id,
 		String name,
 		String email,
 		String crm,
 		Specialty specialty) {
 	public DoctorListDataDTO(Doctor doctor) {
-		this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
+		this(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
 	}
 }
