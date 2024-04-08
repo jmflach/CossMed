0cbdb6470e41cdc3d428df1e8a8bada4c954183f
João Flach <joao.flach@serpro.gov.br>
2024-04-08 09:41:02 -0300

    added error handling for the MethodArgumentNotValidException

--- src/main/java/coss/med/CossMed/infra/ErrorHandler.java
+++ src/main/java/coss/med/CossMed/infra/ErrorHandler.java
@@ -1,6 +1,8 @@
 package coss.med.CossMed.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,17 @@ public class ErrorHandler {
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Void> handle404Error() {
 		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity handle400Error(MethodArgumentNotValidException ex) {
+		var errors = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
+	}
+	
+	private record ValidationErrorData(String field, String message) {
+		public ValidationErrorData(FieldError error) {
+			this(error.getField(), error.getDefaultMessage());
+		}
 	}
 }
