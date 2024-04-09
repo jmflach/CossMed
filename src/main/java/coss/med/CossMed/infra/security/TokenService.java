f009118fb2dd1389f872fbbddaf2f76397563ed5
João Flach <joao.flach@serpro.gov.br>
2024-04-09 11:54:38 -0300

    changed the secret source for the hash algorithm

--- src/main/java/coss/med/CossMed/infra/security/TokenService.java
+++ src/main/java/coss/med/CossMed/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -14,10 +15,13 @@ import coss.med.CossMed.domain.user.User;
 
 @Service
 public class TokenService {
+	
+	@Value("${api.security.token.secret}")
+	private String secret;
 
 	public String generateToken(User user) {
 		try {
-			var algorithm = Algorithm.HMAC256("12345678");
+			var algorithm = Algorithm.HMAC256(secret);
 			return JWT.create()
 					.withIssuer("API Coss.Med")
 					.withSubject(user.getLogin())
